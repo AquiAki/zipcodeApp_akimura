93962fce6fce995bd6b707b56bc2afe14d2df8a2
AquiAki <theorange.01@gmail.com>
2020-12-03 18:29:05 +0900

    Add feature

---
+++ src/results.ts
@@ -0,0 +1,27 @@
+const zipcodeResults:String = {
+
+    /** 郵便番号 */
+    zipcode : string;
+
+    /** 都道府県コード */
+    prefcode : string;
+
+    /** 都道府県名 */
+    address1 : string;
+
+    /** 市区町村名 */
+    address2 : string;
+
+    /** 町域名 */
+    address3 : string;
+
+    /** 都道府県名カナ */
+    kana1 : string;
+
+    /** 市区町村名カナ */
+    kana2 : string;
+
+    /** 町域名カナ */
+    kana3 : string;
+
+};
